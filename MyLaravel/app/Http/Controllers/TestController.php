885902e4b45adef13f89b65f984cf99789a83eb1
Benito <bbxblack@gmail.com>
2021-05-18 11:22:51 +0200

    esercizio live

---
+++ MyLaravel/app/Http/Controllers/TestController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function element($index) {
+        $data = [
+            [
+                'title' => 'titolo 1',
+                'desc' => 'desc 1'                
+            ],
+            [
+                'title' => 'titolo 2',
+                'desc' => 'desc 2'                
+            ],
+            [
+                'title' => 'titolo 3',
+                'desc' => 'desc 3'                
+            ],
+            [
+                'title' => 'titolo 4',
+                'desc' => 'desc 4'                
+            ]
+        ];
+        $elem = $data[$index];
+        return view('pages.element', compact('elem'));
+    }
+}
--- MyLaravel/routes/web.php
+++ MyLaravel/routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', )
+
+Route::get('/elem/{index}', 'TestController@element') 
+    -> name('elem');
